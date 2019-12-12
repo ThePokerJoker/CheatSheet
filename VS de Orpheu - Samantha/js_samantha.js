bb3f0b8662f49da4225fe5963d0cb15c86cda0cf
ThePokerJoker <svalcacio@yahoo.com.br>
2019-12-12 14:00:13 -0200

    prova de orpheu

---
+++ VS de Orpheu - Samantha/js_samantha.js
@@ -0,0 +1,9 @@
+function try() {
+	// body...
+	var x = document.getElementById("hue");
+	var y = document.getElementById("ahia");
+	if (x=!y){
+		alert("Não é o mesmo email.");
+		return invalid;
+	}
+}
